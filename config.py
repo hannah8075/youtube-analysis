cdc780fd5f1a6c376d838119e427c7041ec30ca3
babootaankit <34898171+babootaankit@users.noreply.github.com>
2020-07-07 21:08:50 -0400

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-#api_key = "AIzaSyAdYMS_kXwcstKIIyN41_ZIP5fvgwE6VJA"
-#api_key = "AIzaSyCJ5gq9A-jBggrupFw5ABF4UC9MvCLP_2s"
-api_key = "AIzaSyDPNx43_IAbG3LaEcB34toaSuTDcl5McDI"
-api_key2 = "AIzaSyDEYvOTkctPt9H_HRyd6wCUeD3Uv8x7-Y0"
